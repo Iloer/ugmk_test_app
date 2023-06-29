0fb5b060cbcf77021c83a88402171d7be92c7ef6
Иван Варнавский <ivan.varnavskiy@shtormtech.ru>
2023-06-29 22:53:50 +0300

    fix month name

--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -1,6 +1,5 @@
 export const GetMonthName = (num) => {
-  const date = new Date();
-  date.setMonth(num - 1);
+  const date = new Date(1900, num - 1, 1);
 
   return date.toLocaleString('dafeult', {
     month: 'short',
